f4657c00ec58be204fbd4181d78de2f39ca44eda
maxm-we <77023098+maxm-we@users.noreply.github.com>
2022-08-19 15:52:24 -0500

    path testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -38,6 +38,8 @@ text
 EOF
 ls -la
 pwd
+echo ${SOURCE_DIR:-.}
+echo ${SOURCE_DIR}
 # Sync using our dedicated profile and suppress verbose messages.
 # All other flags are optional via the `args:` directive.
 sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
